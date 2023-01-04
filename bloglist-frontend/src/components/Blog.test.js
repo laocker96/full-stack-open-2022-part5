2e1805d223fbe521099b27047a83ae57aa9986ea
Simone Bergamin <simone.bergamin2a@gmail.com>
2023-01-04 22:44:26 +0100

    Exercise 5.14

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,10 +1,12 @@
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
 import Blog from './Blog'
 
 
-test('a blog render title and author, but not URL or likes by default', () => {
+describe('<Blog />', () => {
+    let container
 
     const user = {
         name: 'Simone Bergamin'
@@ -24,18 +26,40 @@ test('a blog render title and author, but not URL or likes by default', () => {
 
     const mockSetNotificationHandler = jest.fn();
 
-    const { container } = render(<Blog user={user} blog={blog} setBlogs={mockSetBlogsHandler} setNotification={mockSetNotificationHandler} />)
+    beforeEach(() => {
 
-    const visibleDiv = container.querySelector('.visibleBlogContent')
-    expect(visibleDiv).toBeVisible()
+        container = render(<Blog user={user} blog={blog} setBlogs={mockSetBlogsHandler} setNotification={mockSetNotificationHandler} />).container
 
-    const hiddenDiv = container.querySelector('.hiddenBlogContent')
-    expect(hiddenDiv).toBeNull()
+    })
 
-    const div = container.querySelector('.blog')
-    expect(div).toHaveTextContent('Canonical string reduction')
-    expect(div).toHaveTextContent('Edsger W. Dijkstra')
-    expect(div).not.toHaveTextContent('Likes 21')
-    expect(div).not.toHaveTextContent('Simone Bergamin')
-  
+
+    test('a blog renders title and author, but not URL or likes by default', () => {
+
+
+        const visibleDiv = container.querySelector('.visibleBlogContent')
+        expect(visibleDiv).toBeVisible()
+
+        const hiddenDiv = container.querySelector('.hiddenBlogContent')
+        expect(hiddenDiv).toBeNull()
+
+        const div = container.querySelector('.blog')
+        expect(div).toHaveTextContent('Canonical string reduction')
+        expect(div).toHaveTextContent('Edsger W. Dijkstra')
+        expect(div).not.toHaveTextContent('likes 21')
+        expect(div).not.toHaveTextContent('Simone Bergamin')
+
+    })
+
+    test('a blog renders URL and number of likes when the button for details is clicked', async () => {
+
+        const user = userEvent.setup()
+        const button = screen.getByText('view')
+        await user.click(button)
+
+        const hiddenDiv = container.querySelector('.hiddenBlogContent')
+        expect(hiddenDiv).toBeVisible()
+        expect(hiddenDiv).toHaveTextContent('likes 21')
+        expect(hiddenDiv).toHaveTextContent('Simone Bergamin')
+
+    })
 })

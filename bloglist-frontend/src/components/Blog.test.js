d9ad9084dd13ea803b25deaacdf7e55ea981c657
Simone Bergamin <simone.bergamin2a@gmail.com>
2023-01-22 16:02:31 +0100

    Exercise 5.13

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -3,7 +3,12 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 import Blog from './Blog'
+import axios from "axios"
 
+jest.mock("axios");
+
+
+// https://dev.to/zaklaughton/the-only-3-steps-you-need-to-mock-an-api-call-in-jest-39mb
 
 describe('<Blog />', () => {
     let container
@@ -52,8 +57,10 @@ describe('<Blog />', () => {
 
     test('a blog renders URL and number of likes when the button for details is clicked', async () => {
 
+        const mockMethod = jest.fn()
         const user = userEvent.setup()
         const button = screen.getByText('view')
+        button.addEventListener('click', mockMethod)
         await user.click(button)
 
         const hiddenDiv = container.querySelector('.hiddenBlogContent')
@@ -61,5 +68,26 @@ describe('<Blog />', () => {
         expect(hiddenDiv).toHaveTextContent('likes 21')
         expect(hiddenDiv).toHaveTextContent('Simone Bergamin')
 
+    })
+
+
+
+    test('if likes button is called twice also the event handler is called twice', async () => {
+
+        axios.get.mockResolvedValueOnce([blog]);
+        const mockSetDetailsVisible = jest.fn()
+        const mockAddLikes = jest.fn()
+        const user = userEvent.setup()
+
+        const viewButton = screen.getByText('view')
+        viewButton.addEventListener('click', mockSetDetailsVisible)
+        await user.click(viewButton)
+
+        const likeButton = screen.getByText('like')
+        likeButton.addEventListener('click', mockAddLikes)
+        await user.click(likeButton)
+        await user.click(likeButton)
+
+        expect(mockAddLikes.mock.calls).toHaveLength(2)
     })
 })
